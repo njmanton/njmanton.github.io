58878f75f218ebb500a308a19be7819068af4dde
Nick Manton <nick@mantonbradbury.com>
2016-01-10 18:44:38 +0000

    Initial HTML
    Added html page to show scraped data

---
+++ bbfq/stats.js
@@ -0,0 +1,207 @@
+data = {
+  title: "BBFQ statistics",
+  n: 2735,
+  n_unique: 1776,
+  points: 121431.25,
+  quizzes: 126,
+  winning_margin: 12.5,
+  scores: {
+    overall: {
+      max: 67.5,
+      min: 1,
+      ave: 44.4
+    },
+    North: {
+      max: 67.5,
+      min: 1,
+      ave: 44.9
+    },
+    South: {
+      max: 66,
+      min: 6.5,
+      ave: 44.2
+    },
+    East: {
+      max: 66,
+      min: 2.5,
+      ave: 44.0
+    }
+  },
+  names: {
+    "long": [
+      "Dear Santa, All We Would Like For Christmas Is To Not Come Last At The Bigger Boat. All Our Love, Charlie, Adrian, Hugo & James xxx",
+      "I Killed My Lesbian Wife With A Chainsaw, Hung Her On A Meathook And Now I Have A 3-Quiz Deal At Disney",
+      "Paths Of A Full Metal Shining Eyes Wide Spartacus: Or How I Learned To Stop Worrying & Love The Lyndon",
+      "The Derek Zealander Centre For Kids Who Can't Read Good And Want To Learn To Do Other Stuff Good Too"
+    ],
+    "short": [
+      "C-",
+      "Ed"
+    ],
+    ave: 22
+  },
+  victories: {
+    "Mild Peril and some Sexy Scenes": 16,
+    "Analrapists": 12,
+    "Milk was a Bad Choice": 10,
+    "Cheadle in a Haystack": 10
+  },
+  victory_pct: {
+    "Cheadle in a Haystack": 0.59,
+    "Mild Peril and some Sexy Scenes": 0.46,
+    "Analrapists": 0.39,
+    "Milk was a Bad Choice": 0.33
+  },
+  curses: {
+    "fuck": 31,
+    "shit": 28,
+    "piss": 5,
+    "c*nt": 2,
+    "wanker": 3,
+    "quiz": 364
+  },
+  traces: {
+    win_hist: [
+      {
+        type: 'bar',
+        x: [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68],
+        y: [1, 3, 3, 7, 9, 4, 12, 13, 17, 17, 9, 16, 4, 7, 2, 2, 0]
+      }
+    ],
+    scores_over_time: [
+      {
+        type: 'scatter',
+        line: { dash: '8' },
+        name: 'North ave',
+        y: [46.26, 46.72, 43.02, 43.53, 44.25, 45.22, 44.17, 41.74, 43.15, 45.03, 42.9, 45.05, 38.52, 43.31, 42.69, 39.03, 47.15, 42.56, 48.9, 46.86, 46.06, 51.48, 43.62, 47.6, 43.09, 47.06, 41.22, 48.46, 47.81, 39.29, 46.29, 42.74, 40.5, 44.19, 46.53, 45.06, 47.38, 46.65, 47.15, 47.25, 50.52, 40.8, 47.2, 44.52, 43.19,]        
+      },
+      {
+        type: 'scatter',
+        line: { dash: '8' },
+        name: 'South ave',
+        y: [, , , 47.55, 42.41, 43.97, 47.29, 41.63, 41.53, 38.95, 45.45, 42.21, 39.52, 43.52, 45.76, 47.15, 45.93, 45, 49.78, 49.5, 44.87, 47.18, 43.42, 41.1, 42.02, 48.18, 37.44, 42.98, 46.4, 42.94, 47.34, 44.5, 40.59, 43.24, , 42.09, 45.35, 40.02, 45.36, 46.52, 49.89, 42, 42.98, 47.14, ,]
+      },
+      {
+        type: 'scatter',
+        line: { dash: '8' },
+        name: 'East ave',
+        y: [, , , 42.65, 40.86, 43, 39.38, 40.18, 43.44, 40.17, 44, 45.95, 38.95, 39.89, 44.12, 44.08, 43.39, 42.83, 45, 44.83, 42.32, 44.38, 45.53, 43.97, 39.09, 48.15, 44.56, 51.12, 46.81, 44.35, 47.19, 42.85, 41.48, 43.86, 42.45, 42.85, 45.11, 43.85, 46.58, 44.6, 51.4, 40.84, 47.79, 47.41, ,]        
+      },
+      {
+        type: 'scatter',
+        name: 'North',
+        line: { color: '#1f77b4'},
+        y: [59, 61.5, 56.5, 56.5, 61, 62, 63.5, 63.5, 62.5, 62, 59, 67, 58, 57, 57, 61, 61.5, 61.5, 63.5, 63, 58, 63, 62, 63, 60.5, 59, 61, 65, 63.5, 59, 67.5, 61.5, 64, 65, 63.5, 60, 65, 65, 63.5, 64.5, 65.5, 58, 64.5, 60, 63,]
+      },
+      {
+        type: 'scatter',
+        name: 'South',
+        line: { color: '#ff7f0e'},
+        y: [, , , 59.5, 52.5, 58, 53.5, 56, 56.5, 61, 54.5, 54, 54, 62.5, 55, 56.5, 58, 61.5, 60, 66, 61, 63, 60, 60, 63, 57.5, 63, 58, 60, 55, 61.5, 61.5, 58, 63, , 56.5, 59.5, 60, 63, 59, 60.5, 53.5, 65.5, 63.5, ,]
+      },
+      {
+        type: 'scatter',
+        name: 'East',
+        line: { color: '#2ca02c'},
+        y: [, , , 58, 59, 58, 61.5, 57.5, 59, 56, 55, 55, 56, 61, 58, 58.5, 60, 62, 60, 62, 53, 55.5, 59, 60, 59, 64.5, 65.5, 59.5, 66, 55.5, 62.5, 61, 61, 60, 60, 61, 60, 55, 60, 60, 62, 56, 59.5, 58, ,]
+      }
+    ],
+    teams_time_series: [
+      {
+        type: "scatter",
+        name: "North",
+        y: [19, 18, 24, 22, 25, 20, 18, 24, 18, 26, 19, 30, 21, 20, 17, 25, 21, 24, 18, 20, 17, 20, 22, 25, 33, 31, 31, 32, 31, 34, 25, 32, 24, 35, 34, 25, 25, 29, 31, 33, 30, 27, 27, 35, 24]
+      },
+      {
+        type: "scatter",
+        name: "South",
+        y: [, 19, 19, 17, , , 19, 17, 16, 14, 26, 15, 21, 19, 20, 20, 23, 17, 17, 15, 18, 18, 15, 19, 17, 17, , 22, 17, 18, 22, 20, 17, 22, 19, 17, , 20, 21, 22, 21, 18, 17, 21, 18]
+      },
+      {
+        type: "scatter",
+        name: "East",
+        y: [, 21, 24, 20, , , 13, 11, 5, 13, 19, 17, 22, 16, 16, 17, 23, 26, 24, 22, 15, 26, 24, 19, 25, 25, 22, 27, 24, 26, 19, 13, 23, 27, 23, 26, , 27, 20, 24, 20, 20, 25, 17, 18, ]
+      }
+    ],
+    score_time_series: [
+      {
+        type: "bar",
+        y: [46.3, 46.7, 43, 43.5, 44.3, 45.2, 44.2, 41.7, 43.2, 45, 42.9, 45, 38.5, 43.3, 42.7, 39, 47.2, 42.6, 48.9, 46.9, 46.1, 51.5, 43.6, 47.6, 43.1, 47.1, 41.2, 48.5, 47.8, 39.3, 46.3, 42.7, 40.5, 44.2, 46.5, 45.1, 47.4, 46.6, 47.2, 47.3, 50.5, 40.8, 47.2, 44.5, 43.2],
+        name: "average score"
+      },
+      {
+        type: "scatter",
+        y: [59, 61.5, 56.5, 56.5, 61, 62, 63.5, 63.5, 62.5, 62, 59, 67, 58, 57, 57, 61, 61.5, 61.5, 63.5, 63, 58, 63, 62, 63, 60.5, 59, 61, 65, 63.5, 59, 67.5, 61.5, 64, 65, 63.5, 60, 65, 65, 63.5, 64.5, 65.5, 58, 64.5, 60, 63],
+        name: "winning score"
+      }
+    ],
+    average_range: [
+      {
+        type: 'bar',
+        x: [56.5, 52.5, 53],
+        y: ['North', 'South', 'East'],
+        orientation: 'h',
+        name: 'min',
+        marker: {
+          color: '#rgba(0, 0, 0, 0)'
+        }
+      },
+      {
+        type: 'bar',
+        x: [11, 13.5, 13],
+        y: ['North', 'South', 'East'],
+        orientation: 'h',
+        name: 'max'
+      },
+      {
+        type: 'scatter',
+        name: 'average',
+        x: [61.6, 59.1, 59.1],
+        y: ['North', 'South', 'East'],
+        line: { color: 'rgba(0,0,0,0)'},
+        marker: { 
+          color: '#000',
+          symbol: 'diamond',
+          size: 18
+        }
+      }
+    ]
+  },
+  layouts: {
+    ts: {
+      showlegend: true,
+      paper_bgcolor: 'rgba(0, 0, 0, 0)',
+      plot_bgcolor: 'rgba(0, 0, 0, 0)',
+      xaxis: {
+        title: 'Date',
+        tickmode: 'array',
+        tickvals: [0, 12, 24, 36],
+        ticktext: ["2012", "2013", "2014", "2015"],
+        showgrid: false
+      },
+      yaxis: {
+        title: 'Teams',
+        showgrid: true,
+        gridcolor: '#999',
+        zeroline: true,
+        range: [0, 40]
+
+      }
+    },
+    hist: {
+      showlegend: false,
+      paper_bgcolor: 'rgba(0, 0, 0, 0)',
+      plot_bgcolor: 'rgba(0, 0, 0, 0)',
+      xaxis: {
+        range: [52, 68],
+        dtick: 1,
+        title: 'Winning Score'
+      },
+      yaxis: {
+        title: 'Frequency',
+        range: [0, 20],
+        gridcolor: '#999'
+      }
+    }
+  }
+}
